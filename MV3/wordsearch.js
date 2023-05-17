efa4f9dec1f0db5a4d113bc5783352f28ca3955f
Paul Wright <31939113+plwt@users.noreply.github.com>
2023-05-17 20:04:01 +0100

    Update with solution

--- MV3/wordsearch.js
+++ MV3/wordsearch.js
@@ -1,17 +1,17 @@
-function searchWithDuckDuckGo(info, tab) {
+function searchWithGoogle(info, tab) {
   const searchText = info.selectionText;
-  const searchUrl = `https://duckduckgo.com/?q=${encodeURIComponent(searchText)}`;
+  const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchText)}`;
   browser.tabs.create({ url: searchUrl });
 }
 
 browser.contextMenus.create({
-  id: "search-with-duckduckgo",
-  title: "Search with DuckDuckGo",
+  id: "search-with-google",
+  title: "Search with Google",
   contexts: ["selection"]
 });
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "search-with-duckduckgo") {
-    searchWithDuckDuckGo(info, tab);
+  if (info.menuItemId === "search-with-google") {
+    searchWithGoogle(info, tab);
   }
 });
