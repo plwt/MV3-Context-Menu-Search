c2b540b7d8d0d1919daa6a7434907c227e062285
Paul Wright <31939113+plwt@users.noreply.github.com>
2023-05-18 20:12:46 +0100

    Update wordsearch.js

--- MV3/wordsearch.js
+++ MV3/wordsearch.js
@@ -1,15 +1,15 @@
 function searchWithGoogle(info, tab) {
   const searchText = info.selectionText;
-  const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchText)}`;
+  const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(
+    searchText
+  )}`;
   browser.tabs.create({ url: searchUrl });
 }
-
 browser.contextMenus.create({
   id: "search-with-google",
   title: "Search with Google",
-  contexts: ["selection"]
+  contexts: ["selection"],
 });
-
 browser.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "search-with-google") {
     searchWithGoogle(info, tab);
